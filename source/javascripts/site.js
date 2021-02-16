61349937bbcd8fbc5ab9d8d06772fd1dd1a6c5b8
Harley Peddie <harley.peddie@telogis.com>
2021-02-16 22:53:51 +1300

    hardcoding assets from netlify project

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,17 @@
 // This is where it all goes :)
+function myFunction(){
+  var url = "https://go.verizonconnect.com/nz/"+document.getElementById("answer").value+"/"
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               document.getElementById('button').innerHTML = "&check;"
+               window.open(url)
+            }
+            else {
+                document.getElementById('button').innerHTML = "&#8634;"
+            }
+      };
+      xhttp.open("HEAD", url, true);
+      xhttp.send();
+}
